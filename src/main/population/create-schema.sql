38164296f9cd70efe0fd911401f890cc13156530
Jose Carlos <Jose Carlos@dircom.manten.us.es>
2019-12-17 09:35:04 +0100

    Task 004: finished

--- src/main/population/create-schema.sql
+++ src/main/population/create-schema.sql
@@ -279,6 +279,9 @@
     ) engine=InnoDB;
 
     insert into `hibernate_sequence` values ( 1 );
+create index IDXnlv6ege1ixororpblu3lctiev on `application` (`reference_number` desc);
+create index IDX618is0hf6jk8mhi0qeume2hqw on `application` (`creation_moment` desc);
+create index IDX2q2747fhp099wkn3j2yt05fhs on `application` (`status` desc);
 
     alter table `application` 
        add constraint UK_rf84q38qr35ymh5nn0dcxfdue unique (`reference_number`);
